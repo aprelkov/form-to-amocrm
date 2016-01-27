3306087b1276ec6447f33d76d982a423b692cd4e
Alexey Aprelkov <9720009@mail.ru>
2016-01-27 10:32:06 +0300

    Create auth.php

---
+++ auth.php
@@ -0,0 +1,38 @@
+<?php
+#Массив с параметрами, которые нужно передать методом POST к API системы
+$user=array(
+	'USER_LOGIN'=>'name@mail.com', #Ваш логин (электронная почта)
+	'USER_HASH'=>'14dab230a28790072d8a9c266134470' #Хэш для доступа к API (смотрите в профиле пользователя)
+);
+
+$subdomain='subdomain'; #Наш аккаунт - поддомен
+#Формируем ссылку для запроса
+$link='https://'.$subdomain.'.amocrm.ru/private/api/auth.php?type=json';
+$curl=curl_init(); #Сохраняем дескриптор сеанса cURL
+#Устанавливаем необходимые опции для сеанса cURL
+curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-API-client/1.0');
+curl_setopt($curl,CURLOPT_URL,$link);
+curl_setopt($curl,CURLOPT_POST,true);
+curl_setopt($curl,CURLOPT_POSTFIELDS,http_build_query($user));
+curl_setopt($curl,CURLOPT_HEADER,false);
+curl_setopt($curl,CURLOPT_COOKIEFILE,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
+curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
+
+$out=curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
+$code=curl_getinfo($curl,CURLINFO_HTTP_CODE); #Получим HTTP-код ответа сервера
+curl_close($curl); #Заверашем сеанс cURL
+CheckCurlResponse($code);
+/**
+ * Данные получаем в формате JSON, поэтому, для получения читаемых данных,
+ * нам придётся перевести ответ в формат, понятный PHP
+ */
+$Response=json_decode($out,true);
+$Response=$Response['response'];
+if(isset($Response['auth'])) #Флаг авторизации доступен в свойстве "auth"
+	return 'Авторизация прошла успешно';
+return 'Авторизация не удалась';
+
+?>
