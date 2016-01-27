1402cd877912561d12f65d902fba3db9158ac8a0
Alexey Aprelkov <9720009@mail.ru>
2016-01-27 10:35:21 +0300

    Create handler.php

---
+++ handler.php
@@ -0,0 +1,15 @@
+<?php
+error_reporting(-1);
+header('Content-Type: text/html; charset=utf-8');
+$root=__DIR__.DIRECTORY_SEPARATOR;
+
+
+require $root.'prepare.php'; #Здесь будут производиться подготовительные действия, объявления функций и т.д.
+require $root.'auth.php'; #Здесь будет происходить авторизация пользователя
+require $root.'account_current.php'; #Здесь мы будем получать информацию об аккаунте
+require $root.'fields_info.php'; #Получим информацию о полях
+require $root.'contacts_list.php'; #Проверяем, существует ли уже контакт с таким email
+require $root.'lead_add.php'; #Здесь будет происходить добавление сделки
+require $root.'contact_add.php'; #Здесь будет происходить добавление контакта
+
+?>
