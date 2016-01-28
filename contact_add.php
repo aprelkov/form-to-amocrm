6e8eeb0cd2a976021d5a50f7a690068c931d83a2
Alexey Aprelkov <9720009@mail.ru>
2016-01-28 16:51:53 +0300

    Update contact_add.php

--- contact_add.php
+++ contact_add.php
@@ -1,19 +1,20 @@
 <?php
 
+if(empty($contact['id'])) { 
 $contact=array(
 	'name'=>$data['name'],      
 	'linked_leads_id'=>array($lead_id),
-		'custom_fields'=>array(
-			array(
-				'id'=>$custom_fields['EMAIL'],
-				'values'=>array(
-					array(
-						'value'=>$data['email'],
-						'enum'=>'WORK'
-					)
+	'custom_fields'=>array(
+		array(
+			'id'=>$custom_fields['EMAIL'],
+			'values'=>array(
+				array(
+					'value'=>$data['email'],
+					'enum'=>'WORK'
 				)
 			)
 		)
+	)
 );
 
 if(!empty($data['phone']))
@@ -25,7 +26,7 @@ if(!empty($data['phone']))
 				'enum'=>'OTHER'
 			)
 		)
-);
+        );
 
 
 $set['request']['contacts']['add'][] = $contact;
@@ -58,11 +59,48 @@ CheckCurlResponse($code);
 $Response=json_decode($out,true);
 $Response=$Response['response']['contacts']['add'];
 
+echo 'Новая заявка успешно добавлена.';
+
 $output='ID добавленных контактов:'.PHP_EOL;
 
 foreach($Response as $v)
 	if(is_array($v))
 		$output.=$v['id'].PHP_EOL;
 return $output;
+}
+
+else { 
+    $contact=array(
+		'id' => $contact['id'],
+		'linked_leads_id' => array($lead_id),
+		'last_modified' => time(),
+		'name' => $data['name']
+		);
+    
+    $set['request']['contacts']['update'][] = $contact;
+    
+    #Формируем ссылку для запроса
+$link='https://'.$subdomain.'.amocrm.ru/private/api/v2/json/contacts/set';
+$curl=curl_init(); #Сохраняем дескриптор сеанса cURL
+#Устанавливаем необходимые опции для сеанса cURL
+curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-API-client/1.0');
+curl_setopt($curl,CURLOPT_URL,$link);
+curl_setopt($curl,CURLOPT_CUSTOMREQUEST,'POST');
+curl_setopt($curl,CURLOPT_POSTFIELDS,json_encode($set));
+curl_setopt($curl,CURLOPT_HTTPHEADER,array('Content-Type: application/json'));
+curl_setopt($curl,CURLOPT_HEADER,false);
+curl_setopt($curl,CURLOPT_COOKIEFILE,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
+curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
+ 
+$out=curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
+$code=curl_getinfo($curl,CURLINFO_HTTP_CODE);
+$Response=json_decode($out,true);
+$Response2=$Response['response']['contacts']['update'];
+
+echo 'Такой контакт уже существует в CRM. <br>Новая заявка добавлена к нему.';
+}
 
 ?>
