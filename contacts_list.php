0caa265f41b4e4a8f7c6c80713549659ae2f0ac6
Alexey Aprelkov <9720009@mail.ru>
2016-01-29 14:40:07 +0300

    Update contacts_list.php

--- contacts_list.php
+++ contacts_list.php
@@ -1,8 +1,7 @@
 <?php
-
 $link='https://'.$subdomain.'.amocrm.ru/private/api/v2/json/contacts/list?query='.$data['email'];
-$curl=curl_init(); #Сохраняем дескриптор сеанса cURL
-#Устанавливаем необходимые опции для сеанса cURL
+$curl=curl_init(); # Save the cURL session handle
+# Set the necessary options for cURL session
 curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
 curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-API-client/1.0');
 curl_setopt($curl,CURLOPT_URL,$link);
@@ -12,7 +11,7 @@ curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6
 curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
 curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
  
-$out=curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
+$out=curl_exec($curl); # Initiate a request to the API and stores the response to variable
 $code=curl_getinfo($curl,CURLINFO_HTTP_CODE);
 curl_close($curl);
 CheckCurlResponse($code);
@@ -20,5 +19,4 @@ CheckCurlResponse($code);
 $Response=json_decode($out,true);
 $Response=$Response['response']['contacts'];
 $contact=$Response[0];
-
 ?>
