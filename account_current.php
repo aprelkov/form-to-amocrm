ecac4f1cf79a42b3b827dbee09708b13178c52de
Alexey Aprelkov <9720009@mail.ru>
2016-01-29 14:30:25 +0300

    Update account_current.php

--- account_current.php
+++ account_current.php
@@ -1,7 +1,7 @@
 <?php
-$link='https://'.$subdomain.'.amocrm.ru/private/api/v2/json/accounts/current'; #$subdomain уже объявляли выше
-$curl=curl_init(); #Сохраняем дескриптор сеанса cURL
-#Устанавливаем необходимые опции для сеанса cURL
+$link='https://'.$subdomain.'.amocrm.ru/private/api/v2/json/accounts/current'; #$subdomain already announced above
+$curl=curl_init(); # Save the cURL session handle
+# Set the necessary options for cURL session
 curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
 curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-API-client/1.0');
 curl_setopt($curl,CURLOPT_URL,$link);
@@ -11,13 +11,13 @@ curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6
 curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
 curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
  
-$out=curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
+$out=curl_exec($curl); # Initiate a request to the API and stores the response to Variable
 $code=curl_getinfo($curl,CURLINFO_HTTP_CODE);
 curl_close($curl);
 CheckCurlResponse($code);
 /**
- * Данные получаем в формате JSON, поэтому, для получения читаемых данных,
- * нам придётся перевести ответ в формат, понятный PHP
+ * Obtain data in JSON-format, therefore, to obtain the data being read,
+ * we have to translate the answer into a format understood by PHP
  */
 $Response=json_decode($out,true);
 $account=$Response['response']['account'];
