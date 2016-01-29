fa282007cc4c630c6523cd540f2625b6fc2d3863
Alexey Aprelkov <9720009@mail.ru>
2016-01-29 14:58:14 +0300

    Update prepare.php

--- prepare.php
+++ prepare.php
@@ -14,26 +14,26 @@ function CheckCurlResponse($code)
 	);
 	try
 	{
-		#Если код ответа не равен 200 или 204 - возвращаем сообщение об ошибке
+		# If the response code is not equal to 200 or 204 - returns an error message
 		if($code!=200 && $code!=204)
 			throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undescribed error',$code);
 	}
 	catch(Exception $E)
 	{
-		die('Ошибка: '.$E->getMessage().PHP_EOL.'Код ошибки: '.$E->getCode());
+		die('Error: '.$E->getMessage().PHP_EOL.'Error code: '.$E->getCode());
 	}
 }
 
 $data=array(
-	'name'=>isset($_POST['name']) ? $_POST['name'] : 'name',
-	'phone'=>isset($_POST['phone']) ? $_POST['phone'] : 'phone',
-	'email'=>isset($_POST['email']) ? $_POST['email'] : 'email',
+	'name'=>isset($_POST['name']) ? $_POST['name'] : '',
+	'phone'=>isset($_POST['phone']) ? $_POST['phone'] : '',
+	'email'=>isset($_POST['email']) ? $_POST['email'] : '',
 	'add_field_1'=>isset($_POST['add_field_1']) ? $_POST['add_field_1'] : ''
 );
 
-#Если не указано имя или e-mail контакта - уведомляем
+# If no name or e-mail contact - notify
 if(empty($data['name']))
-	die('Не заполнено имя контакта');
+	die('Not filled contact name');
 if(empty($data['email']))
-	die('Не заполнен E-mail контакта');
+	die('Not filled contact email');
 ?>
