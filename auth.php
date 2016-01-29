cad0cb9081e1ae1969bf732e97bb7c95dbb9013d
Alexey Aprelkov <9720009@mail.ru>
2016-01-29 14:33:11 +0300

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,15 +1,15 @@
 <?php
-#Массив с параметрами, которые нужно передать методом POST к API системы
+# An array of parameters that must be passed by the POST method to the API
 $user=array(
-	'USER_LOGIN'=>'name@mail.com', #Ваш логин (электронная почта)
-	'USER_HASH'=>'14dab230a28790072d8a9c266134470' #Хэш для доступа к API (смотрите в профиле пользователя)
+	'USER_LOGIN'=>'name@mail.com', # Your login (email)
+	'USER_HASH'=>'14dab230a28790072d8a9c266134470' # Hash to access the API (see profile)
 );
 
-$subdomain='subdomain'; #Наш аккаунт - поддомен
-#Формируем ссылку для запроса
+$subdomain='subdomain'; # Your account (subdomain)
+# Create a link for request
 $link='https://'.$subdomain.'.amocrm.ru/private/api/auth.php?type=json';
-$curl=curl_init(); #Сохраняем дескриптор сеанса cURL
-#Устанавливаем необходимые опции для сеанса cURL
+$curl=curl_init(); # Save the cURL session handle
+# Set the necessary options for cURL session
 curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
 curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-API-client/1.0');
 curl_setopt($curl,CURLOPT_URL,$link);
@@ -21,18 +21,17 @@ curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6
 curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
 curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
 
-$out=curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
-$code=curl_getinfo($curl,CURLINFO_HTTP_CODE); #Получим HTTP-код ответа сервера
-curl_close($curl); #Заверашем сеанс cURL
+$out=curl_exec($curl); # Initiate a request to the API and stores the response to variable
+$code=curl_getinfo($curl,CURLINFO_HTTP_CODE); # Obtain the HTTP-server response code
+curl_close($curl); # Close cURL session
 CheckCurlResponse($code);
 /**
- * Данные получаем в формате JSON, поэтому, для получения читаемых данных,
- * нам придётся перевести ответ в формат, понятный PHP
+ * Obtain data in JSON-format, therefore, to obtain the data being read,
+ * we have to translate the answer into a format understood by PHP
  */
 $Response=json_decode($out,true);
 $Response=$Response['response'];
-if(isset($Response['auth'])) #Флаг авторизации доступен в свойстве "auth"
-	return 'Авторизация прошла успешно';
-return 'Авторизация не удалась';
-
+if(isset($Response['auth'])) # Flag of authorization is available in the property "auth"
+	return 'Authorization successful';
+return 'Authorization failed';
 ?>
